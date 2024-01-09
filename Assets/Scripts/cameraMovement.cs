ea88fe924c448d46c6267d4b600ebdcdb5a8754a
flopmann <flop5899@gmail.com>
2024-01-09 16:26:23 +0100

    Camera
    Startede camera script

---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    [Tooltip("Object to follow")]
+    public Transform follow;
+    [Tooltip("Max speed")]
+    public float speed;
+
+    private
+
+
+}
