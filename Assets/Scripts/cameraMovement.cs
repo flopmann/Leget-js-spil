da0f527c09017ed27b4de81692b4e063f45f3f12
flopmann <flop5899@gmail.com>
2024-01-19 12:34:39 +0100

    Start scene
    Lavede vore start scene samt begyndte på stormen

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -9,7 +9,34 @@ public class cameraMovement : MonoBehaviour
     [Tooltip("Max speed")]
     public float speed;
 
-    
+    private Vector3 offSet;
+    private Vector3 target;
+    private float currentSpeed = 0f;
 
+
+     void Start()
+    {
+        offSet= transform.position - follow.position;
+    }
+
+    void Update()
+    {
+        Follow();   
+    }
+
+    private void Follow()
+    {
+        target = follow.position + offSet;
+
+
+        float distance = (target - transform.position).magnitude;
+
+        currentSpeed = speed;
+
+
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, target, currentSpeed * Time.deltaTime);
+        transform.position = newPosition;
+
+    }
 
 }
