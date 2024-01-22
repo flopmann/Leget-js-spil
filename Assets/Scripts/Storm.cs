5c7a09166568f2beecdaf0b625db79a10d8b58cd
flopmann <flop5899@gmail.com>
2024-01-22 21:49:19 +0100

    Blockscript
    Startede på blockscripted

--- Assets/Scripts/Storm.cs
+++ Assets/Scripts/Storm.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -33,18 +34,23 @@ public class Storm : MonoBehaviour
 
                 Vector3 position = new Vector3(x, y, z);
                 GameObject instance = GameObject.Instantiate(obj, position, Quaternion.identity);
-                instance.GetComponent<stormMovement>().farm = target.transform;
-                
+                instance.GetComponent<stormMovement>().farm = target.transform;               
                 timer = 0f;
             }
         }
-    }
 
+
+        
+
+
+
+    }
 
     
 
 
-    }
+
+}
 
     
 
