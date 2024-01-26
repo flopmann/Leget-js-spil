89c2d5d1b63739b9ae397fa8aa8d5b0287dd5237
flopmann <flop5899@gmail.com>
2024-01-26 10:01:02 +0100

    carrotsfarmed
    tilføjede så man kan holde styr på hvor mange gulderoder der er blevet høstet.

--- Assets/Scripts/CarrotFarming.cs
+++ Assets/Scripts/CarrotFarming.cs
@@ -7,13 +7,19 @@ public class CarrotFarming : MonoBehaviour
     [SerializeField]
     GameObject Carrot;
 
+    public float carrotsFarmed;
     public Vector3 moveDirection;
     public AudioSource audio_src1;
 
     void OnTriggerEnter(Collider other)
     {
         Carrot.transform.position += moveDirection;
+<<<<<<< Updated upstream
         audio_src1.Play();
+=======
+        carrotsFarmed = carrotsFarmed + 1;
+        Debug.Log(carrotsFarmed);
+>>>>>>> Stashed changes
     }
 
 }
