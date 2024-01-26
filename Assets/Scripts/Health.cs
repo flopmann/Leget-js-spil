ab67213d371316c0d170d2e247f536fd28252294
flopmann <flop5899@gmail.com>
2024-01-26 10:07:24 +0100

    ændrede health systemet

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,20 +8,23 @@ public class Health : MonoBehaviour
     public Text healthText;
     void Start()
     {
-        healthText.text = "health: 3";
+        healthText.text = "health: 100";
     }
     void Update()
     {
 
     
-        if (stormMovement.health == 2)
+        if (stormMovement.health == 75)
         {
-            healthText.text = "health: 2";
+            healthText.text = "health: 75";
         }
-        if (stormMovement.health == 1) 
+        if (stormMovement.health == 50) 
         {
-            healthText.text = "healt: 1";
+            healthText.text = "healt: 50";
         }
-
+        if (stormMovement.health == 25)
+        {
+            healthText.text = "health: 25";
+        }
     }
 }
